6277825a65051ade5306f69e753674e704380ec2
gidon429 <39284292+gidon429@users.noreply.github.com>
2018-05-24 22:11:04 -0400

    Add files via upload

---
+++ shopping_cart.py
@@ -0,0 +1,72 @@
+# shopping_cart.py
+
+products = [
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+
+#product_ids = []
+
+#while True:
+#    product_id = input("Hey please input a product identifier: ")
+#    if product_id == "DONE":
+#        break
+#    else:
+#        product_ids.append(product_id)
+
+#print("--------------")
+#print(product_ids)
+#print("--------------")
+
+import datetime
+now = datetime.datetime.now()
+
+print("---------------------------")
+print("MY GROCERY STORE")
+print("---------------------------")
+print("Web: www.mystore.com")
+print("Phone: 1.123.456.7890")
+print("Checkout Time: " + now.strftime("%Y-%m-%d %H:%M:%S"))
+print("---------------------------")
+print("Shopping Cart Items:")
+
+product_ids = [3, 12, 10, 3, 5, 11]
+
+def matching_product(product_identifier):
+    products_list = [p for p in products if p["id"] == product_identifier]
+    return products_list[0]
+
+raw_total = 0
+
+for pid in product_ids:
+    product = matching_product(pid)
+    raw_total += product["price"]
+    print(str(product["id"]) + " " + product["name"]+ " " + str(product["price"]))
+
+tax = 0.08875
+sales_tax = int(raw_total * tax)
+
+print("---------------------------")
+print("Subtotal: $" + str(round(raw_total,2)))
+print("Plus NYC Sales Tax (" + str(tax * 100) + "%): $" + str(round(int(raw_total) * tax,2)))
+print("Total: $" + str(round(sales_tax,2) + round(raw_total,2)))
+print("---------------------------")
+print("Thanks for your business! Please come again.")
